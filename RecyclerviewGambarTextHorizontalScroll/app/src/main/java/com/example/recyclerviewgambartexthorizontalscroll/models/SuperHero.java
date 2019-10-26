e9114242c5cbe602a6668835ff57f4a7b31a5a2f
Faisal Wafa <faisalwafa10@gmail.com>
2019-10-26 15:40:15 +0700

    finish tugas 2

---
+++ RecyclerviewGambarTextHorizontalScroll/app/src/main/java/com/example/recyclerviewgambartexthorizontalscroll/models/SuperHero.java
@@ -0,0 +1,27 @@
+package com.example.recyclerviewgambartexthorizontalscroll.models;
+
+public class SuperHero {
+    public String heroName;
+    public String gambar;
+
+    public SuperHero(String heroName, String gambar) {
+        this.heroName = heroName;
+        this.gambar = gambar;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public void setHeroName(String heroName) {
+        this.heroName = heroName;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+}
